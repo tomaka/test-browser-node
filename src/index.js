4b6c0b2bfe490bf79e29f562fa33e168eb1cc6cc
Pierre Krieger <pierre.krieger1708@gmail.com>
2020-11-11 13:55:56 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import SubstrateLiteWorker from 'worker-loader!./worker.js';
+
+const worker = new SubstrateLiteWorker();
+worker.onmessage = function (event) {
+    console.log("Received response: ", event.data);
+}
+
+worker.postMessage('{"jsonrpc":"2.0","id":1,"method":"system_name","params":[]}');
---
+++ src/worker.js
@@ -0,0 +1,27 @@
+import * as substrate_lite from 'substrate-lite';
+
+// `null` on startup. Will store the client, once it has finished initializing.
+let client = null;
+
+// Array of RPC queries received before initialization has finished. Will be sent after
+// initialization.
+let unsent_rpcs = [];
+
+onmessage = function (event) {
+    if (client != null) {
+        client.send_json_rpc(event.data)
+    } else {
+        if (unsent_rpcs.length > 100)
+            throw "receiving RPC requests while unsent RPCs queue is already large";
+        unsent_rpcs.push(event.data);
+    }
+};
+
+fetch("polkadot-2251297.json")
+    .then((chain_specs) => chain_specs.text())
+    .then((chain_specs) => substrate_lite.start(chain_specs, (response) => postMessage(response)))
+    .then((c) => {
+        client = c;
+        unsent_rpcs.forEach((rpc) => client.send_json_rpc(rpc));
+        unsent_rpcs = [];
+    });
