2add68c019411306e7641180001d16b2ecc06845
Pierre Krieger <pierre.krieger1708@gmail.com>
2021-03-13 10:25:30 +0100

    Work

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 import * as smoldot from 'smoldot';
+import chain_spec from './westend.json';
 
-fetch("westend.json")
-    .then((chain_specs) => chain_specs.text())
-    .then((chain_specs) => smoldot.start({
-        chain_spec: chain_specs,
-        max_log_level: 3,
-        json_rpc_callback: (response) => {
-            let data = JSON.parse(response);
-            if (!!data.params) {
-                let block_height = parseInt(data.params.result.number);
-                document.getElementById('blockHeight').innerText = block_height;
-            }
+smoldot.start({
+    chain_spec: JSON.stringify(chain_spec),
+    max_log_level: 3,
+    json_rpc_callback: (response) => {
+        let data = JSON.parse(response);
+        if (!!data.params) {
+            let block_height = parseInt(data.params.result.number);
+            document.getElementById('blockHeight').innerText = block_height;
         }
-    }))
+    }
+})
     .then((client) => {
         client.send_json_rpc('{"jsonrpc":"2.0","id":2,"method":"chain_subscribeNewHeads","params":[]}');
     });
