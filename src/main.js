7977dd5dec5ae563c4c1776e302f022d50dca02b
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-10-19 10:14:29 +0200

    Work on client

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 import * as smoldot from '@substrate/smoldot-light';
-import chain_spec from './substrate-node-template.json';
+import chainSpec from './substrate-node-template.json';
 
-const client = smoldot.start({
-    logCallback: (level, target, msg)  => {
-        document.getElementById('logs').innerText += "[" + target + "] " + msg + "\n";
-        console.log(msg);
-    },
-    maxLogLevel: 4,
-    forbidNonLocalWs: true,
-    enableExperimentalWebRTC: true
-});
+let client;
 
-client.addChain({
-    chainSpec: JSON.stringify(chain_spec),
+document.getElementById("start").addEventListener("click", () => {
+    let promise = Promise.resolve();
+    if (client) promise = client.terminate();
+
+    promise.then(() => {
+        document.getElementById('logs').innerText = '';
+
+        client = smoldot.start({
+            logCallback: (level, target, msg)  => {
+                document.getElementById('logs').innerText += "[" + target + "] " + msg + "\n";
+                console.log(msg);
+            },
+            maxLogLevel: 4,
+            forbidNonLocalWs: true,
+            enableExperimentalWebRTC: true
+        });
+
+        chainSpec.bootNodes = [
+            document.getElementById("bootnode").value
+        ];
+    
+        client.addChain({
+            chainSpec: JSON.stringify(chainSpec),
+        }).catch((err) => {
+            document.getElementById('logs').innerText += "Error while creating chain: " + err.toString();
+        })
+    })
+})
+
+document.getElementById("stop").addEventListener("click", () => {
+    if (client)
+        client.terminate().then(() => { client = null })
 })
