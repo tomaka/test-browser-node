08568029ce674672ef1593d64807a60b0772f504
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-10-19 10:04:15 +0200

    WIP

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import * as smoldot from '@substrate/smoldot-light';
-import chain_spec from './westend.json';
+import chain_spec from './substrate-node-template.json';
 
 const client = smoldot.start({
+    logCallback: (level, target, msg)  => {
+        document.getElementById('logs').innerText += "[" + target + "] " + msg + "\n";
+        console.log(msg);
+    },
     maxLogLevel: 4,
     forbidNonLocalWs: true,
+    enableExperimentalWebRTC: true
 });
 
 client.addChain({
     chainSpec: JSON.stringify(chain_spec),
-    jsonRpcCallback: (response) => {
-        let data = JSON.parse(response);
-        if (!!data.params) {
-            let block_height = parseInt(data.params.result.number);
-            document.getElementById('blockHeight').innerText = block_height;
-        }
-    }
 })
-    .then((chain) => {
-        chain.sendJsonRpc('{"jsonrpc":"2.0","id":2,"method":"chain_subscribeNewHeads","params":[]}');
-    });
